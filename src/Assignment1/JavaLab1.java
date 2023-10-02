d4b5a6b7caade1b63bf70fb55d18d121603aad40
Jinsung Park <van0711@gmail.com>
2023-10-01 21:51:46 -0400

    first commit

---
+++ src/Assignment1/JavaLab1.java
@@ -0,0 +1,29 @@
+package Assignment1;
+
+// Course: COMP228(SEC.016) Java Programming
+// Subject: Lab1 Assignment1
+// Student: Jinsung Park(301241866)
+
+public class JavaLab1 {
+    public static void main(String[] args) {
+        Singers singer1 = new Singers();
+        
+        // Display default values
+        System.out.println("Default Values:");
+        singer1.display();
+        
+        // Set values using setters
+        singer1.setId(1);
+        singer1.setName("Taeyeon");
+        singer1.setAddress("Junju, SouthKorea");
+        singer1.setDateOfBirth("3 March 1989");
+        singer1.setAlbumsPublished(13);
+        
+        // Display updated values
+        System.out.println("\nUpdated Values:");
+        singer1.display();
+        
+     }
+}
+
+	
